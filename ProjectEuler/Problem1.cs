5f46ebac7b759e45fefbdeb753fc54e21d68b337
Joseph Aylor <sonik_to0th@hotmail.com>
2016-02-15 23:44:46 -0500

    Init and Problem 1

---
+++ ProjectEuler/Problem1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    class Problem1 : IProblem
+    {
+        public int Below
+        {
+            get; set;
+        }
+
+        public Problem1()
+        {
+            Below = 1000;
+        }
+
+        public int Run()
+        {
+            IEnumerable<int> multiplesOfThreeOrFive = Enumerable.Range(1, Below - 1).Where(n => n % 3 == 0 || n % 5 == 0);
+            return multiplesOfThreeOrFive.Aggregate(0, (sum, n) => sum + n);
+        }
+    }
+}
---
+++ ProjectEuler/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Puzzle Number: ");
+            string puzzleNumber = Console.ReadLine();
+            Assembly assem = typeof(Program).Assembly;
+
+            try
+            {
+                IProblem problem = (IProblem)assem.CreateInstance("ProjectEuler.Problem" + puzzleNumber);
+
+                Console.WriteLine(problem.Run());
+            }
+            catch(System.NullReferenceException ex)
+            {
+                Console.WriteLine("Could Not Find Puzzle Number " + puzzleNumber);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
