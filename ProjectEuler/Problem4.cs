a301776d646f981068b298cdc7f487ed8bb36259
Joseph Aylor <sonik_to0th@hotmail.com>
2016-02-16 22:09:52 -0500

    Rough solution to problem 4

---
+++ ProjectEuler/Problem4.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    class Problem4 : IProblem
+    {
+
+        public int Run()
+        {
+            List<int> firstThreeDigits = Enumerable.Range(100, 900).ToList();
+
+            List<int> secondThreeDigits = Enumerable.Range(100, 900).ToList();
+
+            firstThreeDigits.Reverse();
+            secondThreeDigits.Reverse();
+
+            int maxPalindrome = 0;
+
+            // Too many loops, learn to LINQ
+            while(firstThreeDigits.Count >= 1)
+            {
+                foreach(int secondNumber in secondThreeDigits)
+                {
+                    int product = firstThreeDigits[0] * secondNumber;
+
+                    //EW! Refactor this.
+                    if(product.ToString() == new string(product.ToString().ToCharArray().Reverse().ToArray())
+                        && product > maxPalindrome)
+                    {
+                        maxPalindrome = product;
+                    }
+                }
+                firstThreeDigits.RemoveAt(0);
+                secondThreeDigits.RemoveAt(0);
+            }
+
+            return maxPalindrome;
+        }
+    }
+}
